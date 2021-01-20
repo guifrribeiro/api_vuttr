c97f534ff06ca5e9b373370529e32ac07967f3f0
Guilherme Ferreira <guifrribeiro@gmail.com>
2021-01-20 05:08:47 -0300

    Tools tests

--- test/tools.spec.js
+++ test/tools.spec.js
@@ -3,7 +3,6 @@ const http = require('chai-http')
 const subSet = require('chai-subset')
 
 const app = require('../app')
-const ToolController = require('../src/controllers/ToolController')
 
 chai.use(http)
 chai.use(subSet)
@@ -15,32 +14,62 @@ const ToolSchema = {
   tags: tags => tags
 }
 
+const AuthSchema = {
+  auth: auth => auth,
+  token: token => token
+}
+
 describe('Tools tests', () => {
   it('Store tool', () => {
     chai.request(app.app)
-      .post('/tools')
-      .set('x-access-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMDdjZWI0NjBjOTI3ZDMxNjg3NWJiZCIsImlhdCI6MTYxMTEyNDQ0OSwiZXhwIjoxNjExMjEwODQ5fQ.Uo3-yBYMv5g_JuXob_x5d5-H4y4TXS8_rzJEVGThCJQ')
-      .send({
-        title: 'Guilherme GitHub Page',
-        link: 'https://github.com/guifrribeiro',
-        description: 'Guilherme Ferreira GitHub Page',
-        tags: ["organization", "planning", "collaboration", "writing", "calendar"]
-      })
-      .end((error, response) => {
-        chai.expect(error).to.be.null
-        chai.expect(response).to.have.status(201)
-        chai.expect(response.body).to.containSubset(ToolSchema)
-      })
+    .post('/auth')
+    .send({
+      username: 'guifrribeiro',
+      password: '12345678'
+    })
+    .end((error, response) => {
+      let token = response.body.token
+      chai.expect(error).to.be.null
+      chai.expect(response).to.have.status(200)
+      chai.expect(response.body).to.containSubset([AuthSchema])
+      chai.request(app.app)
+        .post('/tools')
+        .set('x-access-token', token)
+        .send({
+          title: 'Guilherme GitHub Page',
+          link: 'https://github.com/guifrribeiro',
+          description: 'Guilherme Ferreira GitHub Page',
+          tags: ["organization", "planning", "collaboration", "writing", "calendar"]
+        })
+        .end((error, response) => {
+          chai.expect(error).to.be.null
+          chai.expect(response).to.have.status(201)
+          chai.expect(response.body).to.containSubset(ToolSchema)
+        })
+    })
   })
 
   it('Get tools', () => {
     chai.request(app.app)
-      .get('/tools')
-      .set('x-access-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMDdjZWI0NjBjOTI3ZDMxNjg3NWJiZCIsImlhdCI6MTYxMTEyNDQ0OSwiZXhwIjoxNjExMjEwODQ5fQ.Uo3-yBYMv5g_JuXob_x5d5-H4y4TXS8_rzJEVGThCJQ')
-      .end((error, response) => {
-        chai.expect(error).to.be.null
-        chai.expect(response).to.have.status(200)
-        chai.expect(response.body).to.containSubset([ToolSchema])
-      })
+    .post('/auth')
+    .send({
+      username: 'guifrribeiro',
+      password: '12345678'
+    })
+    .end((error, response) => {
+      let token = response.body.token
+      chai.expect(error).to.be.null
+      chai.expect(response).to.have.status(200)
+      chai.expect(response.body).to.containSubset([AuthSchema])
+      chai.request(app.app)
+        .get('/tools')
+        .set('x-access-token', token)
+        .end((error, response) => {
+          chai.expect(error).to.be.null
+          chai.expect(response).to.have.status(200)
+          chai.expect(response.body).to.containSubset([ToolSchema])
+          done()
+        })
+    })
   })
 })
